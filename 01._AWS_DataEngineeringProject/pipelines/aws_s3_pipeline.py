1cd7333c18b7e8e3e76ce7333d3b0ecb15b90330
KevinGastelum <Gastelum.a.kevin@gmail.com>
2024-01-27 17:33:42 -0700

    Update AWS S3 pipeline file

--- 01._AWS_DataEngineeringProject/pipelines/aws_s3_pipeline.py
+++ 01._AWS_DataEngineeringProject/pipelines/aws_s3_pipeline.py
@@ -5,7 +5,7 @@ from utils.constants import AWS_BUCKET_NAME
 from etls.aws_etl import connect_to_s3, create_bucket_if_not_exist, upload_to_s3
 
 def upload_s3_pipeline(ti):
-  file_name = ti.xcom_pull(task_ids='reddit_extraction', key='return_value')
+  file_path = ti.xcom_pull(task_ids='reddit_extraction', key='return_value')
 
   s3 = connect_to_s3()
   create_bucket_if_not_exist(s3, AWS_BUCKET_NAME)
