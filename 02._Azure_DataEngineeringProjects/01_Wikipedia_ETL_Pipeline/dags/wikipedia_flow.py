166df750bab143747ba2eacd90e5d8509ae2a5de
Kevin Gastelum <97716634+KevinGastelum@users.noreply.github.com>
2024-04-08 20:34:27 -0700

    Update Wikipedia Flow Script

--- 02._Azure_DataEngineeringProjects/01_Wikipedia_ETL_Pipeline/dags/wikipedia_flow.py
+++ 02._Azure_DataEngineeringProjects/01_Wikipedia_ETL_Pipeline/dags/wikipedia_flow.py
@@ -21,7 +21,7 @@ dag = DAG(
 extract_data_from_wikipedia = PythonOperator(
   task_id = 'extract_data_from_wikipedia',
   python_callable = extract_wikipedia_data,
-  provide_context = True,
+  # provide_context = True,
   op_kwargs = { 'url': 'https://en.wikipedia.org/wiki/List_of_association_football_stadiums_by_capacity' },
   dag=dag,
 )
