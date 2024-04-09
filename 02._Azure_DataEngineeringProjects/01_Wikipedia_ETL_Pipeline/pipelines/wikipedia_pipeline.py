cdc8124fd18608ce47acc024eaafcffa87a177dc
Kevin Gastelum <97716634+KevinGastelum@users.noreply.github.com>
2024-04-08 20:05:55 -0700

    Azure ETL Wikipedia Data Pipeline

--- 02._Azure_DataEngineeringProjects/01_Wikipedia_ETL_Pipeline/pipelines/wikipedia_pipeline.py
+++ 02._Azure_DataEngineeringProjects/01_Wikipedia_ETL_Pipeline/pipelines/wikipedia_pipeline.py
@@ -15,9 +15,16 @@ def get_wikipedia_page(url):
     print(f'An error ocurred: {e}')
 
 def get_wikipedia_data(html):
-  soup = BeautifulSoup(html, 'htnl.parser')
+  soup = BeautifulSoup(html, 'html.parser')
   table = soup.find_all('table', {'class': 'wikitable sortable'})[0]
 
   table_rows = table.find_all('tr')
 
   return table_rows
+
+def extract_wikipedia_data(**kwargs):
+  url = kwargs['url']
+  html = get_wikipedia_page(url)
+  rows = get_wikipedia_data(html)
+
+  print(rows)
