1fcf8995965b6b6cf29b88ab7d488d2282ff1de8
Kevin Gastelum <97716634+KevinGastelum@users.noreply.github.com>
2024-04-08 20:29:21 -0700

    Update Wikipedia Pipeline Script

--- 02._Azure_DataEngineeringProjects/01_Wikipedia_ETL_Pipeline/pipelines/wikipedia_pipeline.py
+++ 02._Azure_DataEngineeringProjects/01_Wikipedia_ETL_Pipeline/pipelines/wikipedia_pipeline.py
@@ -8,6 +8,7 @@ def get_wikipedia_page(url):
   try:
     response = requests.get(url, timeout=10)
     response.raise_for_status() # Checks if requests was succesful
+
     return response.text
   except requests.RequestException as e:
     print(f'An error ocurred: {e}')
