6aa9e2a6b97b73634671e980728d5bdebdddf92f
Kevin Gastelum <97716634+KevinGastelum@users.noreply.github.com>
2024-04-08 20:23:57 -0700

    Update Wikipedia Pipeline Script

--- 02._Azure_DataEngineeringProjects/01_Wikipedia_ETL_Pipeline/pipelines/wikipedia_pipeline.py
+++ 02._Azure_DataEngineeringProjects/01_Wikipedia_ETL_Pipeline/pipelines/wikipedia_pipeline.py
@@ -1,18 +1,17 @@
 from bs4 import BeautifulSoup
+import requests
 
 
 def get_wikipedia_page(url):
-  import requests
-
   print("Getting wikipedia page...", url)
 
   try:
     response = requests.get(url, timeout=10)
     response.raise_for_status() # Checks if requests was succesful
-
     return response.text
   except requests.RequestException as e:
     print(f'An error ocurred: {e}')
+
 
 def get_wikipedia_data(html):
   soup = BeautifulSoup(html, 'html.parser')
